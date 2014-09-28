5a8d80f221e7e1c84aad8a57cb82b128422c2a0e
p3z <philip.pannenko@gmail.com>
2014-09-28 10:57:53 -0400

    initial commit

---
+++ src/MakeTable.java
@@ -0,0 +1,40 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MakeTable extends HttpServlet {
+
+  private static final long serialVersionUID = 2653008225156340138L;
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    java.io.PrintWriter out = response.getWriter();
+
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Make Table</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>Make Table</h1>");
+    
+    out.println("<table cellspacing='5' cellpadding='5' border='1px'>");
+    for (int i = 1; i < 26; i++) {
+      out.print("<tr>");
+      for (int j = 1; j < 4; j++) {
+        out.print("<th> Row " + i + ", Column " + j + "</th>");
+      }
+      out.println("</tr>");
+    }
+    out.println("</table>");
+
+    out.println("</body>");
+    out.println("</html>");
+
+    out.close();
+  }
+
+}
