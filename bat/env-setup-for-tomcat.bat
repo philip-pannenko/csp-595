a58c0d66e13e252378446ced7faabef5d3ed8682
philippannenko <philip.pannenko@gmail.com>
2014-09-29 16:59:48 -0400

    added ant and update compile bat

---
+++ bat/env-setup-for-tomcat.bat
@@ -0,0 +1,11 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_51
+
+set PATH="C:\Program Files\Java\jdk1.7.0_51\bin";%PATH%
+
+set CLASSPATH=.;C:\servers\apache-tomcat-7.0.34\lib\servlet-api.jar;C:\servers\apache-tomcat-7.0.34\lib\jsp-api.jar;C:\servers\apache-tomcat-7.0.34\lib\el-api.jar;C:\servers\apache-tomcat-7.0.34\lib\commons-beanutils-1.8.3.jar
+
+#set ANT_HOME=C:\servers\apache-tomcat-7.0.34
+
+#set TOMCAT_HOME=C:\servers\apache-tomcat-7.0.34
+
+#set CATALINA_HOME=C:\servers\apache-tomcat-7.0.34
