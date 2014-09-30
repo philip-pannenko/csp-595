0092a72e470e1ce8060626496aae653b8939bc0b
philippannenko <philip.pannenko@gmail.com>
2014-09-29 22:28:31 -0400

    adding forgotten libs.

---
+++ test/src/iit/csp595/service/BasicServiceTest.java
@@ -0,0 +1,18 @@
+package iit.csp595.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicServiceTest {
+
+  @Test
+  public void test() {
+    
+    assert(true);
+    
+    assertTrue(1+1==2);
+    
+  }
+
+}
