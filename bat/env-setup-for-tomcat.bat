6409ab5170211dd1e45c643589c31423837a7a1f
philippannenko <philip.pannenko@gmail.com>
2014-09-29 21:56:24 -0400

    updating ant script and bat file for ease of use

--- bat/env-setup-for-tomcat.bat
+++ bat/env-setup-for-tomcat.bat
@@ -1,11 +1,7 @@
 set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_51
+set ANT_HOME=C:\tools\ant\apache-ant-1.8.4
+set TOMCAT_HOME=C:\servers\apache-tomcat-7.0.34
+set CLASSPATH=.;%TOMCAT_HOME%\lib\servlet-api.jar;%TOMCAT_HOME%\lib\jsp-api.jar;%TOMCAT_HOME%\lib\el-api.jar;%TOMCAT_HOME%\lib\commons-beanutils-1.8.3.jar
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
 
-set PATH="C:\Program Files\Java\jdk1.7.0_51\bin";%PATH%
 
-set CLASSPATH=.;C:\servers\apache-tomcat-7.0.34\lib\servlet-api.jar;C:\servers\apache-tomcat-7.0.34\lib\jsp-api.jar;C:\servers\apache-tomcat-7.0.34\lib\el-api.jar;C:\servers\apache-tomcat-7.0.34\lib\commons-beanutils-1.8.3.jar
-
-#set ANT_HOME=C:\servers\apache-tomcat-7.0.34
-
-#set TOMCAT_HOME=C:\servers\apache-tomcat-7.0.34
-
-#set CATALINA_HOME=C:\servers\apache-tomcat-7.0.34
